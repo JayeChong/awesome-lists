a04ec1a26d2df0eaa259344b061db634e120ad7b
Zhongzhifei <zhongyanWeber@gmail.com>
2017-05-15 20:15:00 +0800

    make a scaffold

---
+++ weatherPWA/js/app.js
@@ -0,0 +1,10 @@
+( 
+function(){
+    // code here
+    /**
+     *  API http://api.jisuapi.com/weather/query?appkey=b545980f676142dd&city=广州
+     *  不仅可以通过城市名查询天气，也可以根据城市id，城市代号，地理经纬度，ip地址来查询。
+     *  
+     */
+}
+)();
